eda28d8003470b669d685d823ad7f54832832b3c
Chris Barlas <barlas.3@osu.edu>
2023-06-07 22:02:05 -0400

    Implemented Machine print_state()

--- Machine.cpp
+++ Machine.cpp
@@ -2,9 +2,44 @@
 #include <iostream>
 #include <cstddef>
 #include <bitset>
+#include <iomanip>
 
 Machine::Machine() {}
 
+using namespace std;
+
 void Machine::print_state() {
-    std::cout << "sp: " << std::bitset<8>(std::to_integer<int>(sp)) << std::endl;
+    print_registers();
+    print_memory();
+}
+
+void Machine::print_registers() {
+    cout << "registers: " << endl;
+    for(int i = 0; i < NUM_REGISTERS; i++) {
+        cout << "V" << hex << i << ": " << general_registers[i] << endl;
+    }
+
+    cout << setw(13) << "Vi: " << vi << endl;
+    cout << "delay timer: " << delay_timer << endl;
+    cout << "sound timer: " << sound_timer << endl;
+    cout << setw(13) << "stack ptr: " << sp << endl;
+    cout << setw(13) << "pc: " << pc << endl;
+}
+
+void Machine::print_memory() {
+    std::cout << "memory: " << std::endl;
+    for(int i = 0; i < MEMORY_TABLE_ROWS; i++) {
+        for(int j = 0; j < MEMORY_TABLE_COLS - 1; j++)
+        {
+            print_memory_table_entry(i, j);
+            std::cout << " | ";
+        }
+        print_memory_table_entry(i, MEMORY_TABLE_COLS - 1);
+        std::cout << std::endl;
+    }
+}
+
+void Machine::print_memory_table_entry(int row, int col) {
+    int entry = row + (col * MEMORY_TABLE_ROWS);
+    std::cout << std::setfill('0') << std::setw(3) << std::hex << entry << ": " << memory[entry];
 }
--- Machine.hpp
+++ Machine.hpp
@@ -1,20 +1,28 @@
 #pragma once
-#include <cstddef>
+#include <bitset>
 
 #define MEMORY_SIZE 4096
 #define NUM_REGISTERS 16
+#define BYTE_SIZE 8
+#define WORD_SIZE 16
+#define MEMORY_TABLE_COLS 8 //Must be in a power of 2
+#define MEMORY_TABLE_ROWS MEMORY_SIZE / MEMORY_TABLE_COLS
 
 using namespace std;
 
 class Machine {
     private:
-        byte memory[MEMORY_SIZE];
-        byte general_registers[NUM_REGISTERS];
-        unsigned short vi;
-        byte delay_timer;
-        byte sound_timer;
-        byte sp;
-        unsigned short pc;
+        bitset<BYTE_SIZE> memory[MEMORY_SIZE];
+        bitset<BYTE_SIZE> general_registers[NUM_REGISTERS];
+        bitset<WORD_SIZE> vi;
+        bitset<BYTE_SIZE> delay_timer;
+        bitset<BYTE_SIZE> sound_timer;
+        bitset<BYTE_SIZE> sp;
+        bitset<WORD_SIZE> pc;
+
+        void print_registers();
+        void print_memory();
+        void print_memory_table_entry(int row, int col);
 
     public:
         Machine();
